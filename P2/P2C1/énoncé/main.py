4b1a01d4686c51a2b332753c7a763050e4ed7187
EauAsis <julien.brouckaert5@gmail.com>
2025-09-09 18:18:16 +0200

    Update main.py

--- P2/P2C1/énoncé/main.py
+++ P2/P2C1/énoncé/main.py
@@ -1,1 +1,24 @@
-# Ecrivez votre code ici !
+nombre1 = input("entre un nombre")
+nombre2 = input("entre un deuxieme nombre")
+resultat = 0
+if not nombre1.isnumeric and not nombre2.isnumeric:
+    print("les nombres sont en chaines de caracteres")
+    raise SystemExit("fin du programme")
+nombre2 = int(nombre2)
+nombre1 = int(nombre1)
+
+operation = str(input("choisissez l'operation + - / *"))
+
+match operation:
+    case "+ ":
+        resultat = nombre1 + nombre2
+    case "- ":
+        resultat = nombre1 - nombre2
+    case "/ ":
+        if nombre2 == 0 :
+            raise SystemExit("division par 0 impossible")
+        else :
+            resultat = nombre1 / nombre2
+    case "* ":
+        resultat = nombre1 * nombre2
+print(f"le resultat est: {round(resultat)}")
